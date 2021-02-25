82cf73cbc80dd6b7db751a2907e46742015f9bdc
jflu <lujunfei@gmail.com>
2021-02-25 18:34:10 +0800

    增加修改模块和exe

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ import psutil
 import ctypes 
 import win32api
 import struct
-
-from ctypes.wintypes import BOOL
-from ctypes.wintypes import DWORD
-from ctypes.wintypes import HANDLE
-from ctypes.wintypes import LPVOID
-from ctypes.wintypes import LPCVOID
-
+from ctypes.wintypes import *
 
 OpenProcess = ctypes.windll.kernel32.OpenProcess
 OpenProcess.restype = HANDLE
@@ -19,7 +13,18 @@ OpenProcess.argtypes = (DWORD, BOOL, DWORD)
 ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
 ReadProcessMemory.restype = BOOL
 ReadProcessMemory.argtypes = (HANDLE, LPCVOID, LPVOID, DWORD, DWORD)
+
+WriteProcessMemory = ctypes.windll.kernel32.WriteProcessMemory
+WriteProcessMemory.restype = BOOL
+WriteProcessMemory.argtypes = (HANDLE, LPVOID, LPCVOID, DWORD, DWORD)
+
+VirtualAllocEx = ctypes.windll.kernel32.VirtualAllocEx
+VirtualAllocEx.restype = LPVOID
+VirtualAllocEx.argtypes = (HANDLE, LPVOID, DWORD, DWORD, DWORD)
+
 PROCESS_ALL_ACCESS =  (0x000F0000|0x00100000|0xFFF)
+VIRTUAL_MEM = (0x1000|0x2000)
+PAGE_READWRITE = 0x00000040
 
 class mine_obj():
     def __init__(self,pid):
@@ -29,30 +34,63 @@ class mine_obj():
         self.bome_count_address = 0x1005194
         self.pid = pid
 
-        self.handle_process =OpenProcess(PROCESS_ALL_ACCESS,False,int(pid))
-        if not self.handle_process:
+        self.handle =OpenProcess(PROCESS_ALL_ACCESS,False,int(pid))
+        if not self.handle:
             print ("error get handle of pid %s" % pid)
             exit(1)
 
-    def read_memory(pid,address,size):
+    def read_memory(self,handle,address,size):
         #buffer = (c_byte * getlenght(type))()
-        dwNumberOfBytesRead = ReadProcessMemory.argtypes[-1]()
-        buffer = ctypes.create_string_buffer(size)
-        result  = ReadProcessMemory(pid, address, buffer, size, ctypes.addressof(dwNumberOfBytesRead) )
+        ##dwNumberOfBytesRead = ReadProcessMemory.argtypes[-1](0)
+        numberOfBytesRead = 0
+        buffer = ctypes.c_int()
+        result  = ReadProcessMemory(handle, address, ctypes.byref(buffer), size,numberOfBytesRead)
         if result is None or result == 0:
             raise Exception("error %s" % ctypes.GetLastError() )
+        '''
+        暂时不知道要传入什么进去才能得到
+        if numberOfBytesRead != size:
+            raise Exception(" read len error get " % numberOfBytesRead )
+        '''
+        return buffer.value
 
     def get_row_data(self):
-        return self.read_memory(self.pid,self.row_address,4)
+        return self.read_memory(self.handle,self.row_address,4)
 
     def get_col_data(self):
-        return self.read_memory(self.pid,self.col_address,4)
+        return self.read_memory(self.handle,self.col_address,4)
 
     def get_game_time_data(self):
-        return self.read_memory(self.pid,self.bome_count_address,4)
+        return self.read_memory(self.handle,self.bome_count_address,4)
+
+    def get_bome_count_data(self):
+        return self.read_memory(self.handle,self.game_time_address,4)
 
     def refresh_data(self):
         return 0
+
+    def allocate(self,hProcess, lpAddress, dwSize, flAllocationType, flProtect):
+        lpBuffer = VirtualAllocEx(hProcess, lpAddress, dwSize, flAllocationType, flProtect)
+        if lpBuffer is None or lpBuffer == 0:
+            raise Exception('Error: %s' %ctypes.GetLastError())
+
+        return lpBuffer
+
+
+    def write_buffer(self,hProcess, lpBaseAddress, lpBuffer, nSize):
+        dwNumberOfBytesWritten = WriteProcessMemory.argtypes[-1]()
+        #result = WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, ctypes.addressof(dwNumberOfBytesWritten))
+        result = WriteProcessMemory(hProcess, lpBaseAddress, lpBuffer, nSize, 0)
+        if result is None or result == 0:
+            raise Exception('Error: %s' % GetLastError())
+
+    def stop_time(self):
+        #6bytes
+        shellcode = "\x90\x90\x90\x90\x90\x90"
+        shellcode_length = 6
+        game_time_add_asm_address = 0x1002FF5
+        arg_address = self.allocate(self.handle,game_time_add_asm_address,shellcode_length,VIRTUAL_MEM,PAGE_READWRITE)
+        whhh=self.write_buffer(self.handle,arg_address,shellcode,shellcode_length)
 
 
 def injectDll(app_name=None):
@@ -64,15 +102,23 @@ def injectDll(app_name=None):
     #dll_len = len(dll_path)
 
     pid_list = psutil.pids()
+    found = False
 
     for pid  in pid_list:
         process_data = psutil.Process(pid)
         if process_data.name() == app_name:
+            found = True
             break
     print ('pid',pid)
 
-    mine_data = mine_obj(pid)
-    print (mine_data.get_row_data() )
+    if found:
+        mine_data = mine_obj(pid)
+        print (mine_data.get_row_data() )
+        print (mine_data.get_col_data() )
+        print (mine_data.get_bome_count_data() )
+        mine_data.stop_time()
+    else:
+        print ("%s not found" % app_name)
 
     #先测试时间不增加。inc替换为nop
 
@@ -92,6 +138,6 @@ def injectDll(app_name=None):
     '''
 
 
-injectDll("winmine1.exe")
+injectDll("winmine.exe")
 
     
